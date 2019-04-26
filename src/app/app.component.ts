2650c95f1876dd3b92d40eaf7b0375cd744a488d
Igo Gomes <contato@igogomes.com.br>
2019-04-25 23:37:14 -0300

    Estudos sobre o framework Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <p>
-      <input #n [(ngModel)]="nome" type="text"/>
-      <span *ngIf="n.value.length<3">
-        O nome deve ter no mínimo 3 caracteres
+      <input #n="ngModel" [(ngModel)]="nome" type="text"
+        required minlength="3"/>
+      <span [hidden]="n.valid" style="color: red">
+        <span [hidden]="!n.errors?.required">
+          O nome é obrigatório
+        </span>
+        <span [hidden]="!n.errors?.minlength">
+          O nome deve ter no mínimo {{n.erros?.minlength?.requiredLength}} caracteres
+        </span>
+        <span><pre>{{n.errors | json}}</pre></span>
       </span>
     </p>
     <p>Nome: {{nome}}</p>

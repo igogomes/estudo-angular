58a100b4542cef9bb88a939f875d5050bce762e6
Igo Gomes <contato@igogomes.com.br>
2019-04-25 23:15:43 -0300

    Estudos sobre o framework Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,40 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div id="box" 
-      (mouseover)="ativa()"
-      (mouseout)="desativa()"
-      (mousemove)="movimenta($event)"
-      [class.sobre]="ativado">
-    </div>
-    <p>Coordenadas: {{coords}}</p>
-    `,
-    styles: [`
-      #box {
-        width: 150px;
-        height: 150px;
-        border: thin solid black;
-      }
-      .sobre  {
-        background-color: red;
-      }
-    `]
+    <p><input #n id="nome" name="nome" type="text"/></p>
+    <p><button (click)="enviar(n.value)">Enviar</button></p>
+    <p>Nome: {{nome}}</p>
+    `
 })
 export class AppComponent {
 
-  ativado: boolean = false;
-  coords: string = '';
-  ativa(): void {
-    this.ativado = true;
-  }
-  desativa(): void {
-    this.ativado = false;
-  }
-
-  movimenta(e): void {
-
-    this.coords = "(" + e.clientX + ", " + e.clientY + ")";
-
-  }
+  nome: string = '';
+ enviar(n): void {
+  this.nome = n;
+ }
 
 }

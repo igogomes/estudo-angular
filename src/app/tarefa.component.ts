75a3ca0a9cf8bde1e213d76d10c3b0ec5c86da5a
Igo Gomes <contato@igogomes.com.br>
2019-04-26 22:52:04 -0300

    Estudos sobre o framework Angular

--- src/app/tarefa.component.ts
+++ src/app/tarefa.component.ts
@@ -9,32 +9,54 @@ import { Component } from '@angular/core';
     `],
     template: `
     
-        <form #f="ngForm" (ngSubmit)="envia()">
+        <form #f="ngForm" (ngSubmit)="envia(f.value)">
 
             <p>
-                <label for="tarefa">Tarefa: </label>>
-                <input #tf="ngModel" type="text [(ngMOdel)] = "tarefa"
+                <label for="tarefa">Tarefa: </label>
+                <input #tf="ngModel" type="text" [(ngModel)] = "t.tarefa"
                     id="tarefa" name = "tarefa" required/>
-                    <span [hidden] = "tf.untouched || tf.valid" style="color: red"
+                    <span [hidden] = "tf.untouched || tf.valid" style="color: red">
                         A tarefa é obrigatória
                     </span>
             </p>
             <p>
                 <label for="prioridade">Prioridade: </label>
-                <select [(ngModel)]="
-
-            <button type="submit">Enviar</button>
+                <select [(ngModel)]="t.prioridade"
+                        id="prioridade" name="prioridade" required>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                </select>
+            </p>
+            <p>
+                <label for="data">Data: </label>
+                    <input #td="ngModel" type="date" [(ngModel)]="t.data">
+                    <span [hidden]="td.untouched || td.valid" style = "color: red">
+                        Data inválida
+                    </span>
+            </p>
+            <input type="hidden" [(ngModel)]="preenchedor" name="preenchedor">
+            <button type="submit" [disabled]="!f.valid">Enviar</button>
 
         </form>
+        <pre>{{t | json}}</pre>
     
     `
 })
 
 export class TarefaComponent {
 
+    preenchedor: string = "Igo";
+
     t:any = {
-        tarefa: '',
-        prioridade: 2
+        tarefa: 'Nova Tarefa',
+        prioridade: 1,
+        data: (new Date()).toISOString().slice(0,10)
+    };
+    envia(dados): void {
+
+        console.log(dados);
+
     }
 
 }
